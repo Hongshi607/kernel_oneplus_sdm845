806ed4c355f8f8ad9cee8ea2afcf473ab3a0a9e3
Hongshi607 <133548286+Hongshi607@users.noreply.github.com>
2023-08-10 22:31:04 +0800

    更新 oem_force_dump.h

--- include/linux/oem_force_dump.h
+++ include/linux/oem_force_dump.h
@@ -8,7 +8,8 @@
 
 #ifndef OEM_FORCE_DUMP_H
 #define OEM_FORCE_DUMP_H
-#define NETLINK_ADB         23
+#ifndef NETLINK_ADB
+#define NETLINK_ADB
 
 extern void oem_check_force_dump_key(unsigned int code, int value);
 extern int oem_get_download_mode(void);
